3bd253c522e660892984344c95a8520396009df9
Nick Kim <nickthuazn@gmail.com>
2021-11-06 15:00:05 -0700

    adding mainloop logic for server

--- node.py
+++ node.py
@@ -6,7 +6,8 @@ import p2pnetwork
 
 class Node(threading.Thread):
     """
-    An instance of a node that is able to connect to and accept connections from other nodes.
+    An instance of a node that is able to connect to and accept connections from other nodes. Each node is part of a
+    p2p network and has a copy of the blockchain.
     """
     def __init__(self, port):
 
@@ -16,3 +17,11 @@ class Node(threading.Thread):
         self.blockchain = Blockchain()
         self.p2p = p2pnetwork(self, socket)
 
+    def setBlockchain(self, newChain):
+        return self.blockchain.replaceChain(newChain)
+
+    def getBlockchain(self):
+        return self.blockchain
+
+    def addBlock(self, newBlock):
+        return self.blockchain.addBlock(newBlock)
--- p2pnetwork.py
+++ p2pnetwork.py
@@ -5,7 +5,28 @@ import threading
 class p2pnetwork():
 
     def __init__(self, node, port):
-        self.addr = ('', port)
+        self.port = port
         self.node = node
         self.peers = {}
-        self.socket = None
+
+    def setServerSocket(self, port):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Using IPv4 protocol with TCP
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # port will be immediately reusable
+        s.bind(('', port))
+        socket.listen(5)
+        return s
+
+    def mainLoop(self):
+        socket = self.setServerSocket(self.port)
+        socket.settimeout(2)
+
+        while True:
+            try:
+                clientSock, clientAddr = socket.accept()
+
+
+            except KeyboardInterrupt:
+                break
+
+            except:
+                continue
